01a59f3774ea289c464fe3fa806fa978cabefafe
Josh Cowan <35657686+joshuajz@users.noreply.github.com>
2024-01-31 02:31:40 -0500

    fixed mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,57 +6,65 @@ function App() {
   const [isNo, setIsNo] = useState(false)
 
   return (
-    <>
+    <div style={{overflow: 'hidden', display:'flex', flexDirection: 'column'}}>
+      <span>
       {isYes ? <Yes /> : 
       isNo ? <No setIsYes={setIsYes} /> :
       
       <>
-      <img src="https://storage.prompt-hunt.workers.dev/clfki0eos0001le08txkrrlbu_1" width="400px"/>
+      <img src="https://storage.prompt-hunt.workers.dev/clfki0eos0001le08txkrrlbu_1" width="300px"/>
       <h1>hey cutie</h1>
       <h2>will you be my valentine?</h2>
       
+      <span>
+
       <button onClick={() => {
         setIsNo(true)
         setIsYes(false)
       }}>no</button> <button onClick={() => {
         setIsYes(true)
-      }}>yes</button> </>} 
-      {/* <Yes /> */}
-      <span style={{position: 'absolute', left: 0, bottom: '10px', right: 0}}>
+      }}>yes</button> 
+      
+
+      </span>
+      </> }
+      </span>
+
+      <span style={{marginTop: '60px'}}>
         <Footer setIsYes={setIsYes} setIsNo={setIsNo}/>
       </span>
-    </>
+    </div>
   )
 }
 
 const Yes = () => {
   return <>
-    <img src="https://i2.wp.com/cdn.dribbble.com/users/605899/screenshots/3858779/animation_by_nikitin891.gif" width="400px"/>
+    <img src="https://i2.wp.com/cdn.dribbble.com/users/605899/screenshots/3858779/animation_by_nikitin891.gif" width="300px"/>
     <h2>WOOOOOOOOOOOOOOO SEEE U THEN 😘</h2>
   </>
 }
-
+//@ts-ignore
 const No = ({setIsYes}) => {
   const [xoffset, setXoffset] = useState(0)
   const [yoffset, setYoffset] = useState(0)
   
   return <>
-    <img src="https://i.imgflip.com/4o2iy4.jpg?a473736" width="400px" />
+    <img src="https://i.imgflip.com/4o2iy4.jpg?a473736" width="300px" />
     <h2>the no button is just for show</h2>
     <button style={{
       position: 'relative',
       left: `${xoffset}px`,
       top: `${yoffset}px`
     }} onClick={() => {
-      setXoffset(Math.random() * (350 - - 350) - 350)
-      setYoffset(Math.random() * (350 - - 350) - 350)
+      setXoffset(Math.random() * (100 - - 100) - 100)
+      setYoffset(Math.random() * (100 - - 100) - 100)
       // console.log(xoffset, yoffset)
     }}>no</button> <button onClick={() => {
         setIsYes(true)
       }}>yes</button>
   </>
 }
-
+//@ts-ignore
 const Footer = (props) => {
   return (
   <>
